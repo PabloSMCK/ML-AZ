8a3df7f52a430c0b05d87f2fcd84beba97f04360
PabloMSC <43505422+PabloMSC@users.noreply.github.com>
2020-09-15 08:16:34 -0300

    a

--- Section 6 - Simple Linear Regression/Python/slr.py
+++ Section 6 - Simple Linear Regression/Python/slr.py
@@ -4,9 +4,9 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 #IMPORT THE DATASET
-dataset = pd.read_csv("Data.csv")
+dataset = pd.read_csv("Salary_Data.csv")
 X = dataset.iloc[:,:-1].values #INDEPENDET VARIABLE/S
-y = dataset.loc[:,'Purchased'].values #DEPENDENT VARIABLE/S
+y = dataset.loc[:,'Salary'].values #DEPENDENT VARIABLE/S
 
 
 #TAKING CARE OF THE MISSING DATA
@@ -26,7 +26,7 @@ y = labelencoder_y.fit_transform(y)"""
 
 #SPLITTING THE DF TO TRAIN AND TEST
 from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 1/3, random_state = 0)
 
 
 #FEATURE SCALLING
@@ -34,3 +34,26 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, rando
 sc_X = StandardScaler()
 X_train  = sc_X.fit_transform(X_train)
 X_test  = sc_X.transform(X_test)"""
+
+from sklearn.linear_model import LinearRegression
+regressor = LinearRegression()
+regressor.fit(X_train, y_train)
+
+#PREDICT THE TEST SET RESULTS
+y_pred = regressor.predict(X_test) #VECTOR WITH PREDICTED VALUES
+
+#VISUALIZING THE TRAINING SET RESULTS
+plt.scatter(X_train, y_train, color = 'red')
+plt.plot(X_train,regressor.predict(X_train), color = 'blue')
+plt.title("Salary vs Experience (Training set)")
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
+
+#VISUALIZING THE TEST SET RESULTS
+plt.scatter(X_test, y_test, color = 'red')
+plt.plot(X_train,regressor.predict(X_train), color = 'blue')
+plt.title("Salary vs Experience (Training set)")
+plt.xlabel('Years of Experience')
+plt.ylabel('Salary')
+plt.show()
