87584953ebc231451df0ded0fd1bae33923d356c
PabloMSC <43505422+PabloMSC@users.noreply.github.com>
2020-09-17 02:48:31 -0300

    a

--- Section 7 - Multiple Linear Regression/Python/multiple_linear_regression.py
+++ Section 7 - Multiple Linear Regression/Python/multiple_linear_regression.py
@@ -1,33 +1,36 @@
-# Multiple Linear Regression
-
-# Importing the libraries
+#IMPORT LIBRARIES
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
-X = dataset.iloc[:, :-1].values
-y = dataset.iloc[:, -1].values
-print(X)
+#IMPORT THE DATASET
+dataset = pd.read_csv("50_Startups.csv")
+X = dataset.iloc[:,:-1].values #INDEPENDET VARIABLE/S
+y = dataset.loc[:,'Profit'].values #DEPENDENT VARIABLE/S
 
-# Encoding categorical data
+
+#TAKING CARE OF THE MISSING DATA
+"""from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform( X[:, 1:3])"""
+
+#ENCODING CATEGORICAL (TRANSFORM COLUMN TEXT VALUES TO NUMBERS)
+"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 from sklearn.compose import ColumnTransformer
-from sklearn.preprocessing import OneHotEncoder
-ct = ColumnTransformer(transformers=[('encoder', OneHotEncoder(), [3])], remainder='passthrough')
-X = np.array(ct.fit_transform(X))
-print(X)
+ct = ColumnTransformer([("Country", OneHotEncoder(), [0])], remainder = 'passthrough')
+X = ct.fit_transform(X)
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)"""
 
-# Splitting the dataset into the Training set and Test set
-from sklearn.model_selection import train_test_split
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)
 
-# Training the Multiple Linear Regression model on the Training set
-from sklearn.linear_model import LinearRegression
-regressor = LinearRegression()
-regressor.fit(X_train, y_train)
+#SPLITTING THE DF TO TRAIN AND TEST
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
 
-# Predicting the Test set results
-y_pred = regressor.predict(X_test)
-np.set_printoptions(precision=2)
-print(np.concatenate((y_pred.reshape(len(y_pred),1), y_test.reshape(len(y_test),1)),1))
+
+#FEATURE SCALLING
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train  = sc_X.fit_transform(X_train)
+X_test  = sc_X.transform(X_test)"""
