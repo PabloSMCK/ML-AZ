3cd48781290de1182c0345bd78366e429cb451af
PabloMSC <43505422+PabloMSC@users.noreply.github.com>
2020-09-23 03:52:20 -0300

    a

--- Section 8 - Polynomial Regression/Python/polynomial_regression.py
+++ Section 8 - Polynomial Regression/Python/polynomial_regression.py
@@ -1,55 +1,76 @@
-# Polynomial Regression
+"""PROBLEM: WE HAVE TO HIRE A PERSON TAHT WANTS 160000 FOR SALARY, WE HAVE TO CHECK IF IN HIS PREVIOUS JOB 
+HE HAD THAT SAME AMOUNT THAT HE IS ASKING, WE ARE GOING TO COMPARE THE RESULTS BETWEEN A LINEAR MODEL AND A
+POLYNOMIAL REGRESSION MODE, HE CLAIMS TO HAVE 6.5 YEARS OF EXPERIENCE 
+"""
 
-# Importing the libraries
+#IMPORT LIBRARIES
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, -1].values
+#IMPORT THE DATASET
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:,1].values #INDEPENDET VARIABLE/S
+y = dataset.iloc[:,2].values #DEPENDENT VARIABLE/S
 
-# Training the Linear Regression model on the whole dataset
+X = X.reshape(len(X),1)
+y = y.reshape(len(y),1)
+#TAKING CARE OF THE MISSING DATA
+"""from sklearn.impute import SimpleImputer
+imputer = SimpleImputer(missing_values = np.nan, strategy = 'mean')
+imputer = imputer.fit(X[:, 1:3])
+X[:, 1:3] = imputer.transform( X[:, 1:3])"""
+
+#ENCODING CATEGORICAL (TRANSFORM COLUMN TEXT VALUES TO NUMBERS)
+"""from sklearn.preprocessing import LabelEncoder, OneHotEncoder
+from sklearn.compose import ColumnTransformer
+ct = ColumnTransformer([("Country", OneHotEncoder(), [0])], remainder = 'passthrough')
+X = ct.fit_transform(X)
+labelencoder_y = LabelEncoder()
+y = labelencoder_y.fit_transform(y)"""
+
+
+#SPLITTING THE DF TO TRAIN AND TEST
+"""from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 0)"""
+
+
+#FEATURE SCALLING
+"""from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train  = sc_X.fit_transform(X_train)
+X_test  = sc_X.transform(X_test)"""
+
+#CREATE THE LINEAR REGRESSION MODEL
 from sklearn.linear_model import LinearRegression
 lin_reg = LinearRegression()
 lin_reg.fit(X, y)
 
-# Training the Polynomial Regression model on the whole dataset
+#CREATE THE POLYNOMIAL REGRESSION MODEL
 from sklearn.preprocessing import PolynomialFeatures
-poly_reg = PolynomialFeatures(degree = 4)
+poly_reg = PolynomialFeatures(degree = 4) #DEGREE ES EL ELEVADO QEU TIENE LA X EN LA EC POLYNOMIAL
 X_poly = poly_reg.fit_transform(X)
+
 lin_reg_2 = LinearRegression()
 lin_reg_2.fit(X_poly, y)
 
-# Visualising the Linear Regression results
+
+#PREDICT THE TEST SET RESULTS
+y_pred = lin_reg.predict(X) #VECTOR WITH PREDICTED VALUES
+
+#VISUALIZING THE TRAINING SET RESULTS LINEAR
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Linear Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.plot(X,y_pred, color = 'blue')
 plt.show()
 
-# Visualising the Polynomial Regression results
+#VISUALIZING THE TRAINING SET RESULTS POLYNOMIAL
 plt.scatter(X, y, color = 'red')
-plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
+plt.plot(X,lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
 plt.show()
 
-# Visualising the Polynomial Regression results (for higher resolution and smoother curve)
-X_grid = np.arange(min(X), max(X), 0.1)
-X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
-plt.show()
 
-# Predicting a new result with Linear Regression
-lin_reg.predict([[6.5]])
+#PREDICTING A VALUE WITH LINEAR REGRESSION EJ: 6.5
+print(lin_reg.predict(([[6.5]])))
 
-# Predicting a new result with Polynomial Regression
-lin_reg_2.predict(poly_reg.fit_transform([[6.5]]))
+#PREDICTING A VALUE WITH POLYNOMIAL REGRESSION EJ: 6.5
+print(lin_reg_2.predict(poly_reg.fit_transform(([[6.5]]))))
--- Section 9 - Support Vector Regression (SVR)/Python/support_vector_regression.py
+++ Section 9 - Support Vector Regression (SVR)/Python/support_vector_regression.py
@@ -1,50 +1,61 @@
-# Support Vector Regression (SVR)
+"""PROBLEM: WE HAVE TO HIRE A PERSON TAHT WANTS 160000 FOR SALARY, WE HAVE TO CHECK IF IN HIS PREVIOUS JOB 
+HE HAD THAT SAME AMOUNT THAT HE IS ASKING, WE ARE GOING TO COMPARE THE RESULTS WITH SUPORT VECTOR MACHINE,
+ HE CLAIMS TO HAVE 6.5 YEARS OF EXPERIENCE 
+ 
 
-# Importing the libraries
+SVR USA FEATURE SCALLING 
+ 
+"""
+
+
+#IMPORT LIBRARIES
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, -1].values
-print(X)
-print(y)
-y = y.reshape(len(y),1)
-print(y)
+#IMPORT THE DATASET
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:,1].values #INDEPENDET VARIABLE/S
+y = dataset.iloc[:,-1].values #DEPENDENT VARIABLE/S
 
-# Feature Scaling
+#TRANSFORM IN A 2D ARRAY
+X = np.reshape(X,(len(X),1))
+y = np.reshape(y,(len(y),1))
+
+
+#SPLITTING THE DF TO TRAIN AND TEST
+"""EN ESTE CASO NO HAY PORQUE HAY POCOS DATOS Y HA YQUE MANTENERLOS"""
+
+#FEATURE SCALLING
 from sklearn.preprocessing import StandardScaler
 sc_X = StandardScaler()
-sc_y = StandardScaler()
-X = sc_X.fit_transform(X)
-y = sc_y.fit_transform(y)
-print(X)
-print(y)
+X  = sc_X.fit_transform(X)
+sc_y = StandardScaler()               
+y  = sc_y.fit_transform(y)
 
-# Training the SVR model on the whole dataset
+#TRAIN THE SVR MODEL
 from sklearn.svm import SVR
 regressor = SVR(kernel = 'rbf')
-regressor.fit(X, y)
+regressor.fit(X,y)
 
-# Predicting a new result
-sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]])))
+#PREDICT A NEW RESULT
+print(sc_y.inverse_transform(regressor.predict(sc_X.transform([[6.5]]))))
 
-# Visualising the SVR results
-plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
-plt.plot(sc_X.inverse_transform(X), sc_y.inverse_transform(regressor.predict(X)), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
+#VISUALIZING THE VSR RESULTS
+plt.scatter(sc_X.inverse_transform(X),sc_y.inverse_transform(y), color = 'red')
+plt.plot(sc_X.inverse_transform(X),sc_y.inverse_transform(regressor.predict(X)), color = 'blue')
+plt.title("Truth or Bluff (SVR)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
 plt.show()
 
-# Visualising the SVR results (for higher resolution and smoother curve)
+#VISUALIZING THE VSR RESULTS WITH PRETIER GRAPHICS
+
 X_grid = np.arange(min(sc_X.inverse_transform(X)), max(sc_X.inverse_transform(X)), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(sc_X.inverse_transform(X), sc_y.inverse_transform(y), color = 'red')
-plt.plot(X_grid, sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid))), color = 'blue')
-plt.title('Truth or Bluff (SVR)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
+plt.scatter(sc_X.inverse_transform(X),sc_y.inverse_transform(y), color = 'red')
+plt.plot(X_grid,sc_y.inverse_transform(regressor.predict(sc_X.transform(X_grid))), color = 'blue')
+plt.title("Truth or Bluff (SVR)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
 plt.show()
