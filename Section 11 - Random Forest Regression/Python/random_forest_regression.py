6f57471ef46917f0f8f072749a83306e169921a6
PabloSMCK <50815736+PabloSMCK@users.noreply.github.com>
2020-10-04 02:29:30 -0300

    a

--- Section 11 - Random Forest Regression/Python/random_forest_regression.py
+++ Section 11 - Random Forest Regression/Python/random_forest_regression.py
@@ -1,29 +1,44 @@
-# Random Forest Regression
+"""PROBLEM: WE HAVE TO HIRE A PERSON TAHT WANTS 160000 FOR SALARY, WE HAVE TO CHECK IF IN HIS PREVIOUS JOB 
+HE HAD THAT SAME AMOUNT THAT HE IS ASKING, WE ARE GOING TO COMPARE THE RESULTS WITH SUPORT VECTOR MACHINE,
+ HE CLAIMS TO HAVE 6.5 YEARS OF EXPERIENCE 
+ 
 
-# Importing the libraries
+DECISION TREE NO USA FEATURE SCALLING 
+ 
+"""
+#IMPORT LIBRARIES
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-# Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
-X = dataset.iloc[:, 1:-1].values
-y = dataset.iloc[:, -1].values
+#IMPORT THE DATASET
+dataset = pd.read_csv("Position_Salaries.csv")
+X = dataset.iloc[:,1].values #INDEPENDET VARIABLE/S
+y = dataset.loc[:,'Salary'].values #DEPENDENT VARIABLE/S
 
-# Training the Random Forest Regression model on the whole dataset
+X = np.reshape(X,(len(X),1))
+#y = np.reshape(y,(len(y),1))
+
 from sklearn.ensemble import RandomForestRegressor
-regressor = RandomForestRegressor(n_estimators = 10, random_state = 0)
-regressor.fit(X, y)
+regressor = RandomForestRegressor(n_estimators=10, random_state=0)
+regressor.fit(X,y)
 
-# Predicting a new result
-regressor.predict([[6.5]])
+print(regressor.predict([[6.5]]))
 
-# Visualising the Random Forest Regression results (higher resolution)
-X_grid = np.arange(min(X), max(X), 0.01)
+#VISUALIZING THE VSR RESULTS WITH PRETIER GRAPHICS
+
+#VISUALIZING THE VSR RESULTS WITH PRETIER GRAPHICS
+
+X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
-plt.scatter(X, y, color = 'red')
-plt.plot(X_grid, regressor.predict(X_grid), color = 'blue')
-plt.title('Truth or Bluff (Random Forest Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
+plt.scatter(X,y, color = 'red')
+plt.plot(X_grid,regressor.predict(X_grid), color = 'blue')
+plt.title("Truth or Bluff (Decision Tree Regression)")
+plt.xlabel("Position Level")
+plt.ylabel("Salary")
 plt.show()
+
+from sklearn.metrics import r2_score
+y_true = y
+y_pred = regressor.predict(X)
+print(r2_score(y_true, y_pred))
